3803d0bc42a19d63c33c5d00aac9b34d694affd4
emmanueloropeza <eoropezag3@gmail.com>
2021-06-03 23:25:27 -0500

    initial commit

---
+++ src/main/java/com/meep/prueba/MeepApplication.java
@@ -0,0 +1,13 @@
+package com.meep.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meep/prueba/MeepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meep.prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
